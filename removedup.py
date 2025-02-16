f1266fe02faeafe6074c07a5eb78b083e005b997
Lê Minh Toàn <109668965+Hissatsu265@users.noreply.github.com>
2025-02-16 19:11:18 +0700

    26 .Create removedup.py

---
+++ removedup.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k=0
+        for i in range(1,len(nums)):
+            if nums[k]!=nums[i]:
+                k=k+1
+                nums[k]=nums[i]
+        # nums[:k+1]
+        return k+1
