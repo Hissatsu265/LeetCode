d6140a6b15564324b2a54ad9e0e26abe5b05125b
Lê Minh Toàn <109668965+Hissatsu265@users.noreply.github.com>
2025-02-16 19:31:02 +0700

    82. Remove Duplicates from Sorted List II

---
+++ removeduplinklist.py
@@ -0,0 +1,20 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        kq=ListNode(0)
+        kq.next=head
+        prev=kq
+        cur=head
+        while cur:
+            if cur.next and cur.val == cur.next.val:
+                while cur.next and cur.val == cur.next.val:
+                    cur = cur.next
+                prev.next = cur.next
+            else:
+                prev = prev.next
+            
+            cur = cur.next
+        return kq.next
